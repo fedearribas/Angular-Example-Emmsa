eeff79ced85b55a2a5583b0440e3cad57e0de048
farribas <farribas@emmsa.net>
2023-02-19 22:14:22 -0300

    added template for product form

--- src/app/projects/project.module.ts
+++ src/app/projects/project.module.ts
@@ -3,16 +3,26 @@ import { PrimengModule } from '../shared/primeng.module';
 import { SharedModule } from '../shared/shared.module';
 import { ProjectReportComponent } from './project-report/project-report.component';
 import { RouterModule } from '@angular/router';
+import { ProjectFormComponent } from './project-form/project-form.component';
 
 @NgModule({
   declarations: [
-    ProjectReportComponent
+    ProjectReportComponent,
+    ProjectFormComponent
   ],
   imports: [
     RouterModule.forChild([
       {
         path: 'projects',
         component: ProjectReportComponent
+      },
+      {
+        path: 'projects/new', 
+        component: ProjectFormComponent
+      },
+      {
+        path: 'projects/:id',
+        component: ProjectFormComponent
       }
     ]),
     SharedModule,
--- src/app/shared/primeng.module.ts
+++ src/app/shared/primeng.module.ts
@@ -14,6 +14,7 @@ import { InputSwitchModule } from 'primeng/inputswitch';
 import { MenuModule } from 'primeng/menu';
 import { MessagesModule } from 'primeng/messages';
 import { MessageModule } from 'primeng/message';
+import { DividerModule } from 'primeng/divider';
 
 @NgModule({
   declarations: [],
@@ -31,7 +32,8 @@ import { MessageModule } from 'primeng/message';
     InputSwitchModule,
     MenuModule,
     MessagesModule,
-    MessageModule
+    MessageModule,
+    DividerModule
   ]
 })
 export class PrimengModule { }
